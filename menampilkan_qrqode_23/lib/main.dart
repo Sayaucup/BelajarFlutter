fbad7d4f356ae5f960c38ce7dce69573dece68bc
Sayaucup <hadiyus321@gmail.com>
2020-05-08 13:11:47 +0700

    Belajar Flutter

---
+++ menampilkan_qrqode_23/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      body: Center(
+        child: QrImage(
+          version: 4,
+          backgroundColor: Colors.transparent,
+          foregroundColor: Colors.black,
+          errorCorrectionLevel: QrErrorCorrectLevel.M,
+          padding: EdgeInsets.all(20),
+          data: 'www.instagram.com/ucup_yh',
+        ),
+      ),
+    ));
+  }
+}
